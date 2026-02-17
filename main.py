21a8032fba1f7541482a7e19b0b6e9057e248de1
whiteswordv <143036168+whiteswordv@users.noreply.github.com>
2026-02-17 15:27:50 -0500

    fix

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, send_file
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return send_file('static/index.html')
+
+
+if __name__ == "__main__":
+    app.run()

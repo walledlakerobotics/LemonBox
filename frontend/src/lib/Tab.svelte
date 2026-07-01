c06edfb1802cccd8510d0b735b7a4e55a42f554c
whiteswordv <143036168+whiteswordv@users.noreply.github.com>
2026-07-01 14:36:08 -0400

    Update Tab.svelte

--- frontend/src/lib/Tab.svelte
+++ frontend/src/lib/Tab.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import type { TabData } from "./tabdata";
+    import type { TabData } from "./tabdata.svelte";
 
     let { tabData }: { tabData: TabData } = $props();
 </script>

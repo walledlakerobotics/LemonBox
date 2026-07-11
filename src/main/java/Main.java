b8bd3bbff612d3b9f6b8e961a830bfd8f7089ff4
whiteswordv <143036168+whiteswordv@users.noreply.github.com>
2026-07-11 15:17:41 -0400

    clean up

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -54,7 +54,6 @@ public class Main {
 
                     // this is directing the root to the html index file.
                     config.routes.get("/", ctx -> ctx.redirect("index.html"));
-                    config.routes.get("/api/connected", ctx -> ctx.json(inst.isConnected()));
 
                     // returns all motors that are connected to the networktables.
                     config.routes.get("/api/motors", ctx -> {

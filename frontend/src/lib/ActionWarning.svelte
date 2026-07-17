9cdd572fab7c67159d48261270b5593950ac8257
BryceHeitzmann <109158364+Dialga156b@users.noreply.github.com>
2026-07-16 23:21:47 -0400

    2 liner

--- frontend/src/lib/ActionWarning.svelte
+++ frontend/src/lib/ActionWarning.svelte
@@ -1,32 +1,36 @@
 <script lang="ts">
-    let { message, onEnable }: { message: string; onEnable: () => void } =
-        $props();
+  let { message, onEnable }: { message: string; onEnable: () => void } =
+    $props();
 </script>
 
 <button id="warning" onclick={onEnable}>
-    <p>{message}</p>
+  <p>{message}</p>
 </button>
 
 <style>
-    #warning {
-        display: flex;
-        flex-direction: column;
+  #warning {
+    display: flex;
+    flex-direction: column;
 
-        text-align: center;
+    text-align: center;
 
-        background-color: var(--warning-bg);
-        color: var(--warning-text);
+    background-color: var(--warning-bg);
+    color: var(--warning-text);
 
-        border: solid;
-        border-width: 1px;
-        border-radius: 5px;
-        border-color: var(--warning-border);
+    border: solid;
+    border-width: 1px;
+    border-radius: 5px;
+    border-color: var(--warning-border);
 
-        position: relative;
-    }
+    position: relative;
+  }
+  #warning p {
+    font-size: 1.5dvh;
+    margin: 10px;
+  }
 
-    #warning:active {
-        background-color: var(--warning-border);
-        color: var(--warning-bg);
-    }
+  #warning:active {
+    background-color: var(--warning-border);
+    color: var(--warning-bg);
+  }
 </style>
--- frontend/src/lib/Warning.svelte
+++ frontend/src/lib/Warning.svelte
@@ -1,20 +1,24 @@
 <script lang="ts">
-    let { message }: { message: string } = $props();
+  let { message }: { message: string } = $props();
 </script>
 
 <div id="warning">
-    <p>{message}</p>
+  <p>{message}</p>
 </div>
 
 <style>
-    #warning {
-        color: var(--warning-text);
-        background-color: var(--warning-bg);
-        border: solid;
-        border-width: 1px;
-        border-color: var(--warning-border);
+  #warning {
+    color: var(--warning-text);
+    background-color: var(--warning-bg);
+    border: solid;
+    border-width: 1px;
+    border-color: var(--warning-border);
 
-        position: relative;
-        text-align: center;
-    }
+    position: relative;
+    text-align: center;
+  }
+  #warning p {
+    font-size: 1.5dvh;
+    margin: 10px;
+  }
 </style>
